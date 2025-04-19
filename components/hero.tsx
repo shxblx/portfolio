e7368e2fad5f9c2b79612c7bba52d6557a7a9b0a
Muhammed Shibli A C <shiblibasheer27@gmail.com>
2025-04-19 19:19:37 +0530

    bug fixes

--- components/hero.tsx
+++ components/hero.tsx
@@ -36,15 +36,15 @@ export default function Hero() {
   return (
     <section
       ref={ref}
-      className="flex flex-col-reverse md:flex-row items-center justify-between min-h-screen pt-16 pb-8 px-4 md:px-8 lg:px-16 gap-0 md:gap-4"
+      className="flex flex-col-reverse md:flex-row items-center justify-between min-h-screen pt-16 pb-8 px-4 md:px-8 lg:px-16 md:gap-4"
     >
       <motion.div
-        className="md:w-1/2 flex justify-center order-2 md:order-1 -mt-8 md:mt-0"
+        className="md:w-1/2 flex justify-center order-2 md:order-1 mt-0 md:mt-0"
         initial={{ opacity: 0, x: -50 }}
         animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }}
         transition={{ duration: 0.8, delay: 0.3 }}
       >
-        <div className="relative w-72 h-[20rem] md:w-[40rem] md:h-[48rem]">
+        <div className="relative w-64 h-64 md:w-[40rem] md:h-[48rem]">
           <Image
             src="/me.png"
             alt="Muhammed Shibli A C"
